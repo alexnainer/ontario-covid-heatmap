fcd115de9b180bcb584767b6a106d35d972b2aeb
Alex Nainer <me@alexnainer.io>
2020-11-07 19:41:47 -0500

    geojson

--- dataParser.js
+++ dataParser.js
@@ -5,20 +5,33 @@ const dataParser = async () => {
 
   const cities = {};
 
-  console.log("total", data);
-  //   console.log("data", data);
   data.result.records.forEach((record) => {
     const phuCity = record.Reporting_PHU_City;
     if (cities[phuCity]) {
-      cities[phuCity]++;
+      cities[phuCity].geoJson.properties.caseNum++;
     } else {
-      cities[phuCity] = 1;
+      cities[phuCity] = {
+        geoJson: {
+          type: "feature",
+          geometry: {
+            type: "Point",
+            coordinates: [
+              record.Reporting_PHU_Longitude,
+              record.Reporting_PHU_Latitude,
+            ],
+          },
+          properties: {
+            id: record.Reporting_PHU,
+            healthUnit: record.Reporting_PHU,
+            city: record.Reporting_PHU_City,
+            caseNum: 1,
+          },
+        },
+      };
     }
   });
-  console.log("cities.length", Object.keys(cities).length);
-  Object.keys(cities).forEach((city) => {
-    console.log(city, cities[city]);
-  });
+  const geoJson = Object.values(cities);
+  console.log("cities", cities);
 };
 
 module.exports = dataParser;
