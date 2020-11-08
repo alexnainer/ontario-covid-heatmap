f80b25cd250b840911d21425c9acd276ce8be1db
Aleks Jugovic <aleks.jugovic@gmail.com>
2020-11-08 01:28:27 -0500

    heatmap elements

--- src/pages/map/MapContainer.js
+++ src/pages/map/MapContainer.js
@@ -23,37 +23,68 @@ class MapContainer extends Component {
     map = new mapboxgl.Map({
       center: [-79.3832, 43.6532],
       zoom: 7,
+      maxZoom: 11,
+      minZoom: 4,
       container: "map",
       style: "mapbox://styles/mapbox/streets-v11",
     });
 
+    
     map.on("load", function () {
       map.resize();
+      
 
       map.addSource("phu", {
         type: "geojson",
         data,
       });
 
+    
+
+      map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'phu',
+        filter: ['has', 'caseNum'],
+        layout: {
+        'text-field': '{caseNum}',
+        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+        'text-size': [
+          'step',
+          ["zoom"],
+          0,
+          7,
+          12
+        ]
+        },
+        paint: {
+          "text-color": "#ffffff"
+        }
+        });
+
       map.addLayer(
         {
           id: "phu-heat",
           type: "heatmap",
           source: "phu",
-          // maxzoom: 9,
           paint: {
-            // Increase the heatmap weight based on frequency and property magnitude
+
             "heatmap-weight": [
               "interpolate",
               ["linear"],
               ["get", "caseNum"],
-              0,
-              0,
-              6,
-              5,
+                100,
+                5,
+                1000,
+                10,
+                5000,
+                20,
+                10000,
+                40,
+                20000,
+                60
             ],
-            // Increase the heatmap color weight weight by zoom level
-            // heatmap-intensity is a multiplier on top of heatmap-weight
+
             "heatmap-intensity": [
               "interpolate",
               ["linear"],
@@ -63,95 +94,126 @@ class MapContainer extends Component {
               9,
               3,
             ],
-            // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-            // Begin color ramp at 0-stop with a 0-transparancy color
-            // to create a blur-like effect.
+
             "heatmap-color": [
               "interpolate",
               ["linear"],
               ["heatmap-density"],
-              0,
-              "rgba(33,102,172,0)",
-              0.2,
-              "rgb(103,169,207)",
-              0.4,
-              "rgb(209,229,240)",
-              0.6,
-              "rgb(253,219,199)",
-              0.8,
-              "rgb(239,138,98)",
-              1,
-              "rgb(178,24,43)",
+                  0.0,
+                  "rgba(0,0,255,0)",
+                  1.0,
+                  "rgb(0,0,255)",
             ],
-            // Adjust the heatmap radius by zoom level
+
             "heatmap-radius": [
               "interpolate",
               ["linear"],
-              ["zoom"],
-              0,
-              2,
-              9,
-              20,
+              ['zoom'],
+              0, 
+              [
+                'interpolate',
+                ['linear'],
+                ["get", "caseNum"],
+                100,
+                20,
+                1000,
+                25,
+                5000,
+                30,
+                10000,
+                35,
+                20000,
+                40
+              ],
+              100, [
+              'interpolate',
+              ['linear'],
+              ["get", "caseNum"],
+              100,
+              18,
+              1000,
+              23,
+              5000,
+              28,
+              10000,
+              33,
+              20000,
+              38
+              ]
             ],
-            // Transition from heatmap to circle layer by zoom level
+
             "heatmap-opacity": [
               "interpolate",
               ["linear"],
               ["zoom"],
-              7,
-              1,
-              9,
               0,
+              6,
+              1,
+              8,
             ],
+
+            'heatmap-opacity': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+                5,
+                1,
+                7,
+                0
+              ]
           },
         },
         "waterway-label"
       );
 
-      // map.addLayer(
-      //   {
-      //     id: "phu-point",
-      //     type: "circle",
-      //     source: "phu",
-      //     minzoom: 7,
-      //     paint: {
-      //       // Size circle radius by earthquake magnitude and zoom level
-      //       "circle-radius": [
-      //         "interpolate",
-      //         ["linear"],
-      //         ["zoom"],
-      //         7,
-      //         ["interpolate", ["linear"], ["get", "caseNum"], 1, 1, 6, 4],
-      //         16,
-      //         ["interpolate", ["linear"], ["get", "caseNum"], 1, 5, 6, 50],
-      //       ],
-      //       // Color circle by earthquake magnitude
-      //       "circle-color": [
-      //         "interpolate",
-      //         ["linear"],
-      //         ["get", "mag"],
-      //         1,
-      //         "rgba(33,102,172,0)",
-      //         2,
-      //         "rgb(103,169,207)",
-      //         3,
-      //         "rgb(209,229,240)",
-      //         4,
-      //         "rgb(253,219,199)",
-      //         5,
-      //         "rgb(239,138,98)",
-      //         6,
-      //         "rgb(178,24,43)",
-      //       ],
-      //       "circle-stroke-color": "white",
-      //       "circle-stroke-width": 1,
-      //       // Transition from heatmap to circle layer by zoom level
-      //       "circle-opacity": ["interpolate", ["linear"], ["zoom"], 7, 0, 8, 1],
-      //     },
-      //   },
-      //   "waterway-label"
-      // );
+      map.addLayer(
+        {
+          id: "phu-point",
+          type: "circle",
+          source: "phu",
+          paint: {
+            "circle-radius": [
+              "interpolate",
+              ["linear"],
+                ['get', 'caseNum'],
+                100,
+                18,
+                1000,
+                23,
+                5000,
+                28,
+                10000,
+                33,
+                20000,
+                38
+            ],
+
+            "circle-color": [
+              "interpolate",
+              ["linear"],
+              ["get", "caseNum"],
+              100,
+              "rgb(0,102,0)",
+              1000,
+              "rgb(0,153,0)",
+              5000,
+              "rgb(204,204,0)",
+              10000,
+              "rgb(255,128,0)",
+              15000,
+              "rgb(255,0,0)",
+              20000,
+              "rgb(204,0,0)",
+            ],
+            "circle-stroke-color": "white",
+            "circle-stroke-width": 0,
+            "circle-opacity": ["interpolate", ["linear"], ["zoom"], 5, 0, 7, 1],
+          },
+        },
+        "waterway-label"
+      );
     });
+  
   }
 
   render() {
