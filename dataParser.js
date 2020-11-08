6a703da58d4ccf63b2040dcf261db245f4be8148
Aleks Jugovic <aleks.jugovic@gmail.com>
2020-11-07 20:22:07 -0500

    backend parsers

--- dataParser.js
+++ dataParser.js
@@ -34,4 +34,48 @@ const dataParser = async () => {
   console.log("cities", geoJson);
 };
 
+const dataParserSchools = async () => {
+  // change below call
+  const { data } = await api.getAllData();
+
+  const schools = {};
+
+  data.result.records.forEach((record) => {
+    const schoolName = record.school;
+    if (schools[school]) {
+      if (record.total_confirmed_cases > schools[school].geoJson.properties.caseNum) {
+        schools[school].geoJson.properties.caseNum = record.total_confirmed_cases;
+      }
+      
+    } else {
+      schools[schoolName] = {
+        geoJson: {
+          type: "feature",
+          geometry:{
+            type:"Point",
+            coordinates : []
+          },
+          properties : {
+              id: schoolName,
+              caseNum: record.total_confirmed_cases
+          }
+        }
+      };
+    }
+  })
+}
+
+
+// const dataParserLtc = async () => {
+//   // change below call
+//   const { data } = await api.getAllData();
+
+//   const longCareHomes = {};
+
+//   data.result.records.forEach((record) => {
+//     const longCareHome = record.LTC_Home;
+//     if (longCareHomes[longCareHome])
+//   })
+
+
 module.exports = dataParser;
